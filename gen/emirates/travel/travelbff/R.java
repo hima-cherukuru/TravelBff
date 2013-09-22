d9669e52304fc6d91ba92200f21ffd922ca2fd12
Himalatha Cherukuru <hima.cherukuru@gmail.com>
2013-09-21 21:33:11 -0700

    1st version of second page

--- gen/emirates/travel/travelbff/R.java
+++ gen/emirates/travel/travelbff/R.java
@@ -24,10 +24,11 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080008;
-        public static final int button1=0x7f080007;
+        public static final int action_settings=0x7f080009;
+        public static final int button1=0x7f080008;
         public static final int editText1=0x7f080001;
         public static final int editText2=0x7f080005;
+        public static final int seekBar1=0x7f080007;
         public static final int textView1=0x7f080000;
         public static final int textView2=0x7f080002;
         public static final int textView3=0x7f080003;
